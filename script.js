1ef5a23e0af0e42fb7fe81a17412cc2ab1c9612f
Michael <109721107+Michaku1V@users.noreply.github.com>
2023-08-14 19:29:21 +0300

    refactor code using classes and modules

--- script.js
+++ script.js
@@ -1,121 +1,147 @@
 'use strict'
 
-const containerEl = document.querySelector('#cards-display')
-const modalEl = document.querySelector('#modal')
-const titleEl = document.querySelector('#newTitle')
-const authorEl = document.querySelector('#newAuthor')
-const pagesEl = document.querySelector('#newPages')
-const readEl = document.querySelector('#newRead')
-const btnOpenForm = document.querySelector('#open-form')
-const btnCloseModal = document.querySelector('#modal-close')
-const btnAddNew = document.querySelector('#add-book')
-const statusEl = document.querySelector('#status-check')
-
-
-let library = [];
-
-function Book(title, author, pages, readStatus) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages; 
-    this.readStatus = readStatus;   
-}
-addBookToLibrary('Lord of the Rings', 'J.R.R.Tolkien', 1000, false);
-addBookToLibrary('Game of Thrones', 'George R.R. Martin', 1500, true);
-
-Book.prototype.changeReadStatus = function () {
-    this.readStatus ? this.readStatus = false : this.readStatus = true;
+class Book {
+    constructor(title, author, pages, readStatus) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.readStatus = readStatus;
+    }
+    changeReadStatus = () => {this.readStatus ? this.readStatus = false : this.readStatus = true;}
 }
 
-function findBook(libraryArray, bookName) {
-    if (libraryArray.length === 0) return;
-    for (const book of libraryArray) {
-        const trimmedTitle = trimString(book.title);
-        if (trimmedTitle === bookName) {
-            return libraryArray.indexOf(book);
+const Library = (function () {
+    let library = [];
+
+    function getLibrary() {
+        return library;        
+    }
+
+    function findBook(libraryArray, bookName) {
+        if (libraryArray.length === 0) return;
+        for (const book of libraryArray) {
+            const trimmedTitle = trimString(book.title);
+            if (trimmedTitle === bookName) {
+                return libraryArray.indexOf(book);
+            };
         };
-    };
-}
-
-function cleanDisplay() {
-    containerEl.innerHTML = ``;
-}
-function displayLibrary() {
-    cleanDisplay()
-    library.forEach((book, i) => {
-        const trimmedTitle = trimString(book.title);
-        const trimmedAuthor = trimString(book.author);
-        containerEl.insertAdjacentHTML('afterbegin', 
-        `<div class="card">
-            <h3>Title: <span id="title">${trimmedTitle}</span></h3>
-            <h3>Author: <span id="author">${trimmedAuthor}</span></h3>
-            <h3>Pages: <span id="pages">${book.pages}</span></h3>
-            <h3>Read status: <span id="status">${book.readStatus?'Finished':'Reading'}</span>
-            <input type='checkbox' class='status-check' id='status-check' ${book.readStatus?'checked':""}>
-            </h3>
-            <button class='btn btn-remove' id='remove'>Remove</button>
-        </div>`
-        );
-    })
-}
-
-function addBookToLibrary(title, author, pages, readStatus) {
-    if (title === '' || author === '' || pages === 0) {
-        alert('Please add title, author and number of pages');
-        return;
-    };
-    for (const book of library) {
-        if (book.title === title && book.author === author) {
-            alert('Book is already in the list');
+    }
+    
+    function addBookToLibrary(title, author, pages, readStatus) {
+        if (title === '' || author === '' || pages === 0) {
+            alert('Please add title, author and number of pages');
             return;
+        };
+        for (const book of library) {
+            if (book.title === title && book.author === author) {
+                alert('Book is already in the list');
+                return;
+            }
+        }
+        const newBook = new Book (title, author, pages, readStatus); 
+        library.push(newBook);
+    }
+    
+    function removeFromLibrary(bookIndex) {
+        library.splice(bookIndex, 1)   
+    }
+    
+    function trimString(str) {
+        const length = 20;
+        const trimmedString = str.length > length ? str.substring(0, length - 3) + "..." : str;
+        return trimmedString;
+    }
+    
+    
+
+    return{addBookToLibrary, removeFromLibrary, trimString, findBook, getLibrary}
+})()
+
+Library.addBookToLibrary('Lord of the Rings', 'J.R.R.Tolkien', 1000, false);
+Library.addBookToLibrary('Game of Thrones', 'George R.R. Martin', 1500, true);
+
+const DisplayController = (function () {
+    const libraryArr = Library.getLibrary()
+    const library = Library;
+    //Cache DOM
+    const containerEl = document.querySelector('#cards-display')
+    const modalEl = document.querySelector('#modal')
+    const titleEl = document.querySelector('#newTitle')
+    const authorEl = document.querySelector('#newAuthor')
+    const pagesEl = document.querySelector('#newPages')
+    const readEl = document.querySelector('#newRead')
+    const btnOpenForm = document.querySelector('#open-form')
+    const btnCloseModal = document.querySelector('#modal-close')
+    const btnAddNew = document.querySelector('#add-book')
+
+    //Bind Events
+    btnOpenForm.addEventListener('click', toggleModalVisibility)
+    btnCloseModal.addEventListener('click', toggleModalVisibility)
+    document.addEventListener('click', handleModal)
+    btnAddNew.addEventListener('click', displayBooks)
+    document.addEventListener('click', switchStatus)
+    document.addEventListener('click', removeCurrentBook)
+
+    function init() {
+        displayLibrary()
+    }
+    init()
+ 
+    function displayLibrary() {
+        cleanDisplay()
+        libraryArr.forEach((book, i) => {
+            const trimmedTitle = library.trimString(book.title);
+            const trimmedAuthor = library.trimString(book.author);
+            containerEl.insertAdjacentHTML('afterbegin', 
+            `<div class="card">
+                <h3>Title: <span id="title">${trimmedTitle}</span></h3>
+                <h3>Author: <span id="author">${trimmedAuthor}</span></h3>
+                <h3>Pages: <span id="pages">${book.pages}</span></h3>
+                <h3>Read status: <span id="status">${book.readStatus?'Finished':'Reading'}</span>
+                <input type='checkbox' class='status-check' id='status-check' ${book.readStatus?'checked':""}>
+                </h3>
+                <button class='btn btn-remove' id='remove'>Remove</button>
+            </div>`
+            );
+        })
+    }
+
+    function cleanDisplay() {
+        containerEl.innerHTML = ``;
+    }
+
+    function toggleModalVisibility() {
+        modalEl.classList.toggle('hidden');
+    }
+
+    function handleModal(e) {
+        if (e.target === modalEl) toggleModalVisibility();
+    }
+
+    function displayBooks(e) {
+        e.preventDefault();
+        library.addBookToLibrary(titleEl.value, authorEl.value, +pagesEl.value, readEl.checked);
+        displayLibrary();
+        titleEl.value = '';
+        authorEl.value = '';
+        pagesEl.value = '';
+    }
+
+    function removeCurrentBook(e) {
+        if (e.target.id === 'remove') {
+            const currentBookTitle = e.target.parentNode.childNodes[1].lastChild.textContent;
+            const currentBookIndex = library.findBook(libraryArr, currentBookTitle);
+            library.removeFromLibrary(currentBookIndex);
+            displayLibrary();
         }
     }
-    const newBook = new Book (title, author, pages, readStatus); 
-    library.push(newBook);
-    displayLibrary();
-    titleEl.value = '';
-    authorEl.value = '';
-    pagesEl.value = '';
-    toggleModalVisibility();
-}
 
-function removeFromLibrary(bookIndex) {
-    library.splice(bookIndex, 1)   
-}
-
-function trimString(str) {
-    const length = 20;
-    const trimmedString = str.length > length ? str.substring(0, length - 3) + "..." : str;
-    return trimmedString;
-}
-
-function toggleModalVisibility() {
-    modalEl.classList.toggle('hidden');
-}
-
-// BIND EVENTS
-btnOpenForm.addEventListener('click', toggleModalVisibility)
-btnCloseModal.addEventListener('click', toggleModalVisibility)
-document.addEventListener('click', (e) => {
-    if (e.target === modalEl) toggleModalVisibility();
-    // ADD NEW BOOK
-    if (e.target === btnAddNew) {
-        e.preventDefault();
-        addBookToLibrary(titleEl.value, authorEl.value, +pagesEl.value, readEl.checked);
-        console.log(library)  
+    function switchStatus(e) {
+        if (e.target.id === 'status-check') {   
+            const currentBookTitle = e.target.parentNode.parentNode.childNodes[1].lastChild.textContent;
+            const currentBook = libraryArr[library.findBook(libraryArr, currentBookTitle)];
+            currentBook.changeReadStatus();
+            displayLibrary();
+        }
     }
-    // REMOVE CURRENT BOOK
-    if (e.target.id === 'remove') {
-        const currentBookTitle = e.target.parentNode.childNodes[1].lastChild.textContent;
-        const currentBookIndex = findBook(library, currentBookTitle);
-        removeFromLibrary(currentBookIndex);
-        displayLibrary();
-    }
-   // CHANGE READ STATUS
-    if (e.target.id === 'status-check') {
-        const currentBookTitle = e.target.parentNode.parentNode.childNodes[1].lastChild.textContent;
-        const currentBook = library[findBook(library, currentBookTitle)];
-        currentBook.changeReadStatus();
-        displayLibrary();
-    }
-})
+})()
